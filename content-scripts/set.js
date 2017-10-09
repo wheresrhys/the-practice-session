be284686db4370a016fe26772d8fe081e8fe4175
Rhys Evans <wheresrhys@gmail.com>
2017-10-09 21:50:39 +0100

    better styles

--- content-scripts/set.js
+++ content-scripts/set.js
@@ -1,4 +1,8 @@
 const setId = /sets\/(\d*)/.exec(window.location.href)[1];
 const heading = document.querySelector('h2')
-practicifySet(heading, setId)
+practicifySet(heading, setId);
+
+[...heading.querySelectorAll('button')].slice(0, 2).forEach(button => button.addEventListener('click', () => {
+	setTimeout(() => window.location = `/members/${memberId}/sets`, 200);
+}))
 exposeData(null, heading)
